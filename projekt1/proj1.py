ae4e36d7fa810dd5a5c8156b7159637b6f66acd3
s22847 <s22847@pjwstk.edu.pl>
2023-03-20 13:57:38 +0100

    mass center of circle added

--- projekt1/proj1.py
+++ projekt1/proj1.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 cap = cv.VideoCapture('movingball.mp4')
 kernel = np.ones((5,5),np.uint8)
 while(1):
@@ -7,6 +8,7 @@ while(1):
     _, frame = cap.read()
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    img = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
 
     # lower boundary RED color range values; Hue (0 - 20)
     lower1 = np.array([0, 100, 10])
@@ -15,20 +17,49 @@ while(1):
     # upper boundary RED color range values; Hue (160 - 180)
     lower2 = np.array([160,100,20])
     upper2 = np.array([179,255,255])
+
+    #light pink
+    #lower3 = np.array([150,10,20])
+    #upper3 = np.array([170,70,255])
     
-    lower_mask = cv.inRange(hsv, lower1, upper1)
-    upper_mask = cv.inRange(hsv, lower2, upper2)
- 
-    full_mask = lower_mask + upper_mask;
+    mask1 = cv.inRange(hsv, lower1, upper1)
+    mask2 = cv.inRange(hsv, lower2, upper2)
+    #mask3 = cv.inRange(hsv, lower3, upper3)
+
+    full_mask = mask1 + mask2;
 
     # Bitwise-AND mask and original image
-    res = cv.bitwise_and(frame,frame, mask= full_mask)
-    opening = cv.morphologyEx(res, cv.MORPH_OPEN, kernel)
-    opAndClos = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+    mask = cv.bitwise_and(frame,frame, mask= full_mask)
+    opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
+    dilation = cv.dilate(opening,kernel,iterations = 1)
+    opAndClos = cv.morphologyEx(dilation, cv.MORPH_CLOSE, kernel)
+
+    edges = cv.Canny(opAndClos,100,200)
+    edges = cv.dilate(edges,kernel,iterations = 1)
+    edges = cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel)
+
+    gray_img = cv.cvtColor(opAndClos, cv.COLOR_BGR2GRAY)
+
+    # convert the grayscale image to binary image
+    ret,thresh = cv.threshold(gray_img,127,255,0)
+ 
+    # calculate moments of binary image
+    M = cv.moments(thresh)
+ 
+    # calculate x,y coordinate of center
+    cX = int(M["m10"] / M["m00"])
+    cY = int(M["m01"] / M["m00"])
+ 
+    # put text and highlight the center
+    cv.circle(frame, (cX, cY), 5, (255, 100, 100), -1)
+    cv.putText(frame, "centroid", (cX - 25, cY - 25),cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 100, 100), 2)
     
-    cv.imshow('frame',frame)
+    # display the image
+    cv.imshow('edge',edges)
     cv.imshow('mask',full_mask)
     cv.imshow('res',opAndClos)
+    cv.imshow("Image", frame)
+    
     k = cv.waitKey(5) & 0xFF
     if k == 27:
         break
